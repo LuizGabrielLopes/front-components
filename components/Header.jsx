3783522ba9f5167767aa5d4120974efbb93bc8dc
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-03-27 11:38:54 -0300

    feat: add an input at header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from "../styles/Header.module.css";
+import Input from "./Input";
+import { icons } from "../data/icons";
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <h1>theme</h1>
-            <nav className={styles.nav}>
+            <nav className={styles.navigation}>
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/">About</a></li>
@@ -12,6 +14,7 @@ export default function Header() {
                 </ul>
             </nav>
 
+            <Input type="text" placeholder="Search" value="" />
 
         </header>
     );
