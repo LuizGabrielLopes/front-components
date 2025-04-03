b7b133f4ad71929e05ee9e3219f00b086aad90d0
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-04-02 23:54:15 -0300

    feat: updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,11 @@ import styles from "../styles/Header.module.css";
 import Input from "./Input";
 import { icons } from "../data/icons";
 
+
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1>theme</h1>
+            <a href="https://play.google.com/store/apps/details?id=com.supercell.clashroyale&pcampaignid=web_share"><img src="/logoClashRoyale.png" alt="Clash Royale Logo" className={styles.logo} /></a>
             <nav className={styles.navigation}>
                 <ul>
                     <li><a href="/">Home</a></li>
