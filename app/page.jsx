4f227251c7c93d523b4f29838f0aabe0e2190d19
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2025-04-03 10:46:22 -0300

    feat: create footer

--- app/page.jsx
+++ app/page.jsx
@@ -11,6 +11,7 @@ export default function Home() {
       <Section />
       <Container />
       <CategorieContainer />
+      <Container />
     </div>
   )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; 2025 Clash Royale. Todos os direitos reservados.</p>
+        </footer>
+    );
+}
